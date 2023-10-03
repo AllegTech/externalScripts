ed1e31adf360b29eec4bc3ae76e2ecb0868747cb
Zackery Schwermer <schwermer@gmail.com>
2023-10-03 08:26:27 -0500

    Update ExampleScript.ps1

--- ExampleScript.ps1
+++ ExampleScript.ps1
@@ -1,1 +1,3 @@
+# URL = https://raw.githubusercontent.com/AllegTech/externalScripts/main/ExampleScript.ps1
+# ExampleCommand iex (Invoke-WebRequest https://raw.githubusercontent.com/AllegTech/externalScripts/main/ExampleScript.ps1).content
 get-service | ft
