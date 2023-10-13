bd7bb33a05004a4b870c637b60c955ea5e5342c2
Nick Noonan <nsnoonan@gmail.com>
2023-10-13 08:22:02 -0500

    Update ExampleScript.ps1

--- ExampleScript.ps1
+++ ExampleScript.ps1
@@ -1,3 +1,20 @@
-# URL = https://raw.githubusercontent.com/AllegTech/externalScripts/main/ExampleScript.ps1
-# ExampleCommand iex (Invoke-WebRequest https://raw.githubusercontent.com/AllegTech/externalScripts/main/ExampleScript.ps1).content
+# $url = https://raw.githubusercontent.com/AllegTech/externalScripts/main/ExampleScript.ps1
+# ExampleCommand iex (Invoke-WebRequest $url).content
 get-service | ft
+
+# here is how you would handle a script
+if ([Net.SecurityProtocolType]::Tls) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls
+}
+if ([Net.SecurityProtocolType]::Tls11) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls11
+}
+if ([Net.SecurityProtocolType]::Tls12) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+}
+if ([Net.SecurityProtocolType]::Tls13) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls13
+}
+$moduleUrl = "https://raw.githubusercontent.com/AllegTech/externalScripts/main/security/InsecureClientProtocols.psm1"
+(new-object Net.WebClient).DownloadString($moduleUrl) | Invoke-Expression
+Test-InsecureClientProtocols
